c66ae1cbf9039c84be0b6dc2370908f9452a8735
Khaine Sun <c2030076@newcastle.ac.uk>
2022-12-12 00:53:31 +0000

    first push

---
+++ CSC8503CoreClasses/StateMachine.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace NCL {
+	namespace CSC8503 {
+		class State;	// Predeclare the classes we need
+		class StateTransition;
+		// Typedefs to make life easier !
+		typedef std::multimap<State*, StateTransition*> TransitionContainer;
+		typedef TransitionContainer::iterator TransitionIterator;
+
+		class StateMachine	{
+		public:
+			StateMachine();
+			virtual ~StateMachine(); //made it virtual!
+
+			void AddState(State* s);
+			void AddTransition(StateTransition* t);
+
+			virtual void Update(float dt); //made it virtual!
+
+		protected:
+			State * activeState;
+
+			std::vector<State*> allStates;
+			TransitionContainer allTransitions;
+		};
+	}
+}
